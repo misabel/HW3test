95be3cbb82922721b41f65cb9ba8debaa49e8380
misabel <maria-irizarry@uiowa.edu>
2013-10-07 21:41:50 -0700

    final version?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_current_user
   def set_current_user
      @current_user||=session[:session_token] && User.find_by_session_token(session[:session_token])
   end
